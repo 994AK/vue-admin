a61b698c443b42c0fed4fae56fd8b46949d7b35c
994AK <1505398065@qq.com>
2022-02-11 11:59:08 +0800

    feat: layout布局

---
+++ src/layout/components/AppMain.vue
@@ -0,0 +1,12 @@
+<template>
+
+  <div>Hello world</div>
+</template>
+
+<script setup>
+import {} from 'vue'
+</script>
+
+<style lang='scss' scoped>
+
+</style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,11 +1,22 @@
 <template>
-  <div>Hello world</div>
+  <div class="app-wrapper">
+    <!--  左边 menu -->
+    <sidebar class="sidebar-container"></sidebar>
+    <!--  顶部 navBer  -->
+    <div class="main-container">
+      <div class="fixed-header">
+        <navbar />
+      </div>
+      <app-main></app-main>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import AppMain from './components/AppMain'
 import {} from 'vue'
 </script>
 
-<style lang='scss' scoped>
-
-</style>
+<style lang="scss" scoped></style>
